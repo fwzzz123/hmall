6282632919d4bf3d0bbc1bb9d1ff754f307e4a09
wei FU <fwv587@gmail.com>
2024-05-30 23:15:03 +0800

    origin

---
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -0,0 +1,17 @@
+package com.hmall.api.client;
+
+
+import com.hmall.api.dto.ItemDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+//请求方式，请求路径，请求参数，返回值类型，不用去实现，会被OpenFeign动态实现
+@FeignClient("item-service")
+public interface ItemClient {
+    @GetMapping("/items")
+    List<ItemDTO>queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+}
