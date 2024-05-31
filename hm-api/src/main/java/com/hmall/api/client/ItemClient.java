b37d3e71d4b57ed5c620b19bab300d59e1e7ee14
wei FU <fwv587@gmail.com>
2024-05-31 22:57:04 +0800

    depart_trade

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -1,17 +1,27 @@
 package com.hmall.api.client;
 
 
+
 import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
 import java.util.List;
 
 //请求方式，请求路径，请求参数，返回值类型，不用去实现，会被OpenFeign动态实现
+
 @FeignClient("item-service")
 public interface ItemClient {
+
     @GetMapping("/items")
-    List<ItemDTO>queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+    List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+
+    @PutMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
+
 }
